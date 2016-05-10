ac2043c0bf7fca091a22e4a5e39f6c26a6e2a2b0
am-gooru <ashish@gooru.org>
2016-05-10 09:31:05 +0530

    Startup hardening

--- src/main/java/org/gooru/nucleus/auth/gateway/bootstrap/ServerVerticle.java
+++ src/main/java/org/gooru/nucleus/auth/gateway/bootstrap/ServerVerticle.java
@@ -1,14 +1,15 @@
 package org.gooru.nucleus.auth.gateway.bootstrap;
 
-import io.vertx.core.AbstractVerticle;
-import io.vertx.core.http.HttpServer;
-import io.vertx.ext.web.Router;
-
 import org.gooru.nucleus.auth.gateway.constants.ConfigConstants;
 import org.gooru.nucleus.auth.gateway.routes.RouteConfiguration;
 import org.gooru.nucleus.auth.gateway.routes.RouteConfigurator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.http.HttpServer;
+import io.vertx.ext.web.Router;
 
 /**
  * Starts the HTTP gateway for auth.
@@ -23,7 +24,7 @@ public class ServerVerticle extends AbstractVerticle {
     private static final Logger LOG = LoggerFactory.getLogger(ServerVerticle.class);
 
     @Override
-    public void start() throws Exception {
+    public void start(Future<Void> voidFuture) throws Exception {
         LOG.info("Starting auth ServerVerticle...");
         final HttpServer httpServer = vertx.createHttpServer();
         final Router router = Router.router(vertx);
@@ -39,17 +40,16 @@ public class ServerVerticle extends AbstractVerticle {
         LOG.info("Http server starting on port {}", port);
         httpServer.requestHandler(router::accept).listen(port, result -> {
             if (result.succeeded()) {
-                LOG.info("HTTP Server started successfully");
+                voidFuture.complete();
+                LOG.info("Http Server started successfully");
             } else {
-                // Can't do much here, Need to Abort. However, trying to exit
-                // may have
-                // us blocked on other threads that we may have spawned, so we
-                // need to
-                // use
-                // brute force here
-            LOG.error("Not able to start HTTP Server", result.cause());
-            Runtime.getRuntime().halt(1);
-        }
-    }   );
+                // Can't do much here, Need to Abort. However, trying to exit may have
+                // us blocked on other threads that we may have spawned, so we need to
+                // use brute force here
+                LOG.error("Not able to start HTTP Server", result.cause());
+                voidFuture.fail(result.cause());
+                Runtime.getRuntime().halt(1);
+            }
+        });
     }
 }
